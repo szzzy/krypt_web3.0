b604ed636519c9f7e5f999537f1b42ef7b6d3e5a
szzzy <1925861393@qq.com>
2022-11-29 03:12:11 -0500

    part of Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,60 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
 const Welcome = () => {
+	const connectWallet = () => {
+
+	}
+
+
 	return (
-		<h1>Welcome</h1>
+		<div className="flex w-full justify-center items-center">
+		  <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+		    
+		    <div className="flex flex-1 justify-start flex-col md:mr-10">
+		      <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+		        Send Crypto <br /> across the world
+		      </h1>
+		      <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+		        Explore 
+		      </p>
+		      <button
+		        type="button"
+		        onClick={connectWallet}
+		        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+		      >
+		        <p className="text-white text-base font-semibold">Connect Wallet</p>
+		      </button>
+		      <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+		        <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+		          Reliability
+		        </div>
+		        <div className={companyCommonStyles}>Security</div>
+		        <div className={`rounded-tr-2xl ${companyCommonStyles}`}>
+		          Ethereum
+		        </div>
+		        <div className={`rounded-bl-2xl ${companyCommonStyles}`}>
+		          Web 3.0
+		        </div>
+		        <div className={companyCommonStyles}>Low fees</div>
+		        <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+		          Blockchain
+		        </div>
+		      </div>
+		    </div>
+
+		    <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+		      <div className="p-3 justify-end">
+		      </div>
+		    </div>
+
+		  </div>
+		</div>
 	);
 }
 
