3a2de37b60e1df885dfb46689b57db509ffd50df
szzzy <1925861393@qq.com>
2022-11-30 03:17:26 -0500

    Welcome.jsx and Loader.jsx done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -30,14 +30,14 @@ const Welcome = () => {
 
 	return (
 		<div className="flex w-full justify-center items-center">
-		  <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+		  <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
 		    
-		    <div className="flex flex-1 justify-start flex-col md:mr-10">
+		    <div className="flex flex-1 justify-start flex-col mf:mr-10">
 		      <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
 		        Send Crypto <br /> across the world
 		      </h1>
 		      <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-		        Explore 
+		        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
 		      </p>
 		      <button
 		        type="button"
@@ -64,7 +64,7 @@ const Welcome = () => {
 		      </div>
 		    </div>
 
-		    <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+		    <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
 		      <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
 		        <div className="flex justify-between flex-col w-full h-full">
 		          
@@ -97,7 +97,7 @@ const Welcome = () => {
 
 		        <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-		        {false ? (
+		        {true ? (
 		          <Loader />
 		        ) : (
 		            <button
