2ea81fa4def7f40aa475feecb1f3e445845caac3
szzzy <1925861393@qq.com>
2022-12-13 09:38:40 -0500

    part of context

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -19,3 +19,45 @@ const getEthereumContract = () => {
 	});
 }
 
+export const TransactionProvider = ({ children }) => {
+	
+	const [CurrentAccount, setCurrentAccount] = useState("");
+
+
+	const checkIfWalletIsConnected = async () => {
+		if(!ethereum) return alert("Please install Metamask");
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+		
+		if(accounts.length) {
+			setCurrentAccount(accounts[0]);
+			//getAllTransactions();
+		}
+		console.log(accounts);
+	}
+
+	const connectWallet = async () => {
+		try {
+			if(!ethereum) return alert("Please install Metamask");
+
+			const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error);
+
+			throw new Error("No ethereum object.")
+		}
+		
+	}
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, [])
+
+	return (
+		<TransactionContext.Provider value={{ connectWallet: connectWallet }}>
+			{children}
+		</TransactionContext.Provider>
+	)
+}
