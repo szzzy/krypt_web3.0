216650fcee5c92ae0236117a02c2c3471413de35
szzzy <1925861393@qq.com>
2022-12-14 04:42:12 -0500

    part 2 done and call contract function done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -22,10 +22,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 )
 
 const Welcome = () => {
-	const { connectWallet } = useContext(TransactionContext);
+	const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
 
 
-	const handleSubmit = () => {
+	const handleSubmit = (e) => {
+		const { addressTo, amount, keyword, message } = formData;
+		
+		e.preventDefault();
+
+		if(!addressTo || !amount || !keyword || !message) return;
+
+		sendTransaction();
 
 	}
 
@@ -41,13 +48,16 @@ const Welcome = () => {
 		      <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
 		        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
 		      </p>
-		      <button
-		        type="button"
-		        onClick={connectWallet}
-		        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-		      >
-		        <p className="text-white text-base font-semibold">Connect Wallet</p>
-		      </button>
+		      {!currentAccount && (
+		      	<button
+		          type="button"
+		          onClick={connectWallet}
+		          className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+		      	>
+		          <p className="text-white text-base font-semibold">Connect Wallet</p>
+		      	</button>
+		      )}
+
 		      <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
 		        <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
 		          Reliability
@@ -92,14 +102,14 @@ const Welcome = () => {
 		      </div>
 
 		      <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-		        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-		        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-		        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-		        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+		        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+		        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+		        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+		        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
 		        <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-		        {true ? (
+		        {false ? (
 		          <Loader />
 		        ) : (
 		            <button
